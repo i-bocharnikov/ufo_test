da31db4b929f43be920c051bdbacda06d5fbbf76
igor <i.bocharnikov@andersenlab.com>
2018-11-14 12:19:03 +0200

    UFOAPP-17.5/separate styles with jsx for UFOTooltip

--- src/components/UFOTooltip/index.js
+++ src/components/UFOTooltip/index.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
 import {
   Modal,
-  StyleSheet,
   View,
   Text,
   ViewPropTypes,
@@ -12,77 +11,10 @@ import {
 } from 'react-native';
 import PropTypes from 'prop-types';
 
-import { colors, textThemes } from './../utils/theme';
+import styles, { TAIL_SIZE, TAIL_INDENT } from './styles';
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('screen');
-const TAIL_SIZE = 20;
-const TAIL_INDENT = 20;
 const STATUS_BAR_HEIGHT = Platform.OS === 'android' ? StatusBar.currentHeight : 0;
-
-const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-
-  container: {
-    backgroundColor: colors.BG_DEFAULT,
-    paddingHorizontal: 20,
-    paddingVertical: 12,
-    width: '55%',
-    borderRadius: 6,
-    shadowOffset: {width: 1,  height: 1},
-    shadowRadius: 0.2,
-    shadowColor: 'black',
-    shadowOpacity: 0.4,
-    elevation: 2
-  },
-
-  label: {
-    ...textThemes.SP_REGULAR,
-    color: colors.TEXT_INVERT_COLOR,
-    fontSize: 12,
-    lineHeight: 15
-  },
-
-  tail: {
-    borderWidth: TAIL_SIZE / 2,
-    borderColor: colors.BG_DEFAULT,
-    position: 'absolute'
-  },
-
-  tailBottom: {
-    bottom: -TAIL_SIZE,
-    borderBottomColor: 'transparent',
-    borderLeftColor: 'transparent',
-    borderStartColor: 'transparent',
-    borderRightColor: 'transparent',
-    borderEndColor: 'transparent'
-  },
-
-  tailTop: {
-    top: -TAIL_SIZE,
-    borderBottomColor: colors.BG_DEFAULT,
-    borderTopColor: 'transparent',
-    borderLeftColor: 'transparent',
-    borderStartColor: 'transparent',
-    borderRightColor: 'transparent',
-    borderEndColor: 'transparent'
-  },
-
-  tailCenter: {
-    alignSelf: 'center'
-  },
-
-  tailLeft: {
-    left: TAIL_INDENT
-  },
-
-  tailRight: {
-    right: TAIL_INDENT
-  }
-});
 
 export default class UFOTooltip extends PureComponent {
   constructor() {
