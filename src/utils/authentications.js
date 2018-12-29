6c69a272d9113eba8bab7cf525bc01e21da74b9f
igor <i.bocharnikov@andersenlab.com>
2018-12-29 15:27:10 +0200

    UGD-295/fixed utiles

--- src/utils/authentications.js
+++ src/utils/authentications.js
@@ -1,13 +1,13 @@
 import SInfo from 'react-native-sensitive-info';
 
-const AUTHENTICATION_TOKEN_STORAGE_KEY: string = "AT";
-const AUTHENTICATION_UUID_STORAGE_KEY: string = "AU";
-const AUTHENTICATION_PASSWORD_STORAGE_KEY: string = "AP";
+const AUTHENTICATION_TOKEN_STORAGE_KEY: string = 'AT';
+const AUTHENTICATION_UUID_STORAGE_KEY: string = 'AU';
+const AUTHENTICATION_PASSWORD_STORAGE_KEY: string = 'AP';
 
 const options = {
   sharedPreferencesName: 'myUFOPrefs',
   keychainService: 'myUFOKeychain'
-}
+};
 
 export async function getAuthenticationTokenFromStore() {
   return await SInfo.getItem(AUTHENTICATION_TOKEN_STORAGE_KEY, options);
@@ -37,5 +37,5 @@ export async function clearAuthenticationsFromStore() {
   await SInfo.deleteItem(AUTHENTICATION_TOKEN_STORAGE_KEY, options);
   await SInfo.deleteItem(AUTHENTICATION_UUID_STORAGE_KEY, options);
   await SInfo.deleteItem(AUTHENTICATION_PASSWORD_STORAGE_KEY, options);
-  return
+  return;
 }
