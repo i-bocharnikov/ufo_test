0523c70d9f6b23e2877923985ac20232a74fece6
igor <i.bocharnikov@andersenlab.com>
2019-01-02 18:11:16 +0200

    UFD-298 - fixed

--- src/components/common/UFOIcon.js
+++ src/components/common/UFOIcon.js
@@ -1,16 +1,13 @@
 import React, { PureComponent } from 'react';
-import { Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import PropTypes from 'prop-types';
 
 import { sizes, colors, icons } from './../../utils/global';
 
-const isIos = Platform.OS === 'ios';
-
 export default class UFOIcon extends PureComponent {
   render() {
     const { icon, color, inverted, size, style } = this.props;
-    const name = `${isIos ? 'ios-' : 'md-'}${icon && icon.name ? icon.name : icons.WRONG}`;
+    const name = `md-${icon && icon.name ? icon.name : icons.WRONG}`;
     const fontSize = size && size.fontSize ? size.fontSize : sizes.MASSIVE.fontSize;
     const iconColor = color ? color : inverted ? colors.ACTIVE : colors.ICON;
 
