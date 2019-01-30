7a8aae8ea559566c1d8885fbce9cb3064a360c4a
Stany Blanvalet <stany.blanvalet@ufodrive.com>
2019-01-30 11:22:32 +0100

    Remove trans for term key as it was used by fr device

--- src/utils/i18n/fr.js
+++ src/utils/i18n/fr.js
@@ -1,12 +1,5 @@
 import otaKeyNativeErrors from './fr_otaKeyNativeErrors';
 
 export default {
-  home: {
-    'welcome': 'Bonjour {{user.reference}}, voici les prochaine actions?'
-  },
-  term: {
-    'confirmContractTitle': 'Enter \'{{strKey}}\' to confirm',
-    'confirmContractKeyString': 'Je suis d\'accord'
-  },
   otaKeyNativeErrors
 };
