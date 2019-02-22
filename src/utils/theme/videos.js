ae36d2e164645e995f8a7e414a9ca376f07e27dd
Stany Blanvalet <stany.blanvalet@ufodrive.com>
2019-02-22 01:05:47 +0100

    reuse same video

--- src/utils/theme/videos.js
+++ src/utils/theme/videos.js
@@ -1,4 +1,4 @@
 export default {
   homeScreenBg: require('./../../assets/videos/landscape_example_HD.mp4'),
-  launchScreenBg: require('./../../assets/videos/portrait_example.mp4')
+  launchScreenBg: require('./../../assets/videos/landscape_example_HD.mp4')
 };
